0aee137405633811f11837490aeee6047504f157
afzalfaizi <afzaalm993@gmail.com>
2024-05-02 12:05:17 +0500

    Path Variable

--- students_crud/main.py
+++ students_crud/main.py
@@ -7,21 +7,25 @@ app = FastAPI()
 
 students = [{
     "userName":"Muhammad Afzal",
-    "rollNo": 126016
+    "rollNo": 126016,
+    "course": "Web Development",
+    "semester": 3,
+    "year": 2024,
+    "email": "afzaalm993@gmail.com",
+    "phone": "0300-1234567",
+    "address": "House # 10, Street # 1, Nankana Sahib"
 },
             {
     "userName":"Mian Haroon",
-    "rollNo": 126017
+    "rollNo": 126017,
+    "course": "Web Development",
+    "semester": 3,
+    "year": 2024,
+    "email": "mianharoon@gmail.com",
+    "phone": "0300-1234567",
+    "address": "House # 12, Street # 3, Faisalabad"
 },
-            {
-                "userName":"Jawad Ahmad",
-                "rollNo": 126018
-            },
-            {
-                "userName":"Shaikh Zaid",
-                "rollNo": 126019
-            }
-            ]
+]
 
 @app.get("/")
 def getStudents():
@@ -34,11 +38,12 @@ def getStudents():
 def getRollNo(rollNo):
     print("The Roll No of Muhammad Afzal (Student of Web Development) is", rollNo)
     return rollNo 
+# Write a programme that will return the data of the student whose Roll no is given by the user using path variable.
 
 # Query Parameter 
 # Receiving Data From Front-End to the Back-End through Query Parameter
 
-@app.get("/getfromdata")
+@app.get("/getformdata")
 def getRollNo(userName:str, rollNo:str):
     print("Student Name is", userName, "&", "Student Roll no is", rollNo)
     return "Students Addmissions form"
